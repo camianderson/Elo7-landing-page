13a508fd35a32da1bf1350d6a470a36d26a00d78
Cami Anderson <camidantas@icloud.com>
2023-01-10 10:01:58 -0500

    Atualizar componente App

--- src/App.js
+++ src/App.js
@@ -1,37 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
 
 export default App;
-
-
-<div>
-  <div>
-    <h1>Trabalhe no Elo7</h1>
-  </div>
-  <div>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-    <span></span>
-    <a></a>
-  </div>
-</div>
