27a51a855b39c838ceaa0c7a89ea19c636fe7697
Cami Anderson <camidantas@icloud.com>
2023-01-11 10:30:42 -0500

    Criar e estilizar o componente Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,14 @@
-import './Header.scss';
-
 const Header = () => {
     return (
-        <div>
-            header
+        <div className='header-container'>
+            <div className='header-container--imagem'>
+                <h1 className='title'>Trabalhe no Elo7</h1>
+            </div>
+            <div className='header-container--descrição'>
+                <p className='descrição'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ut efficitur ex, at malesuada ligula. Pellentesque odio ipsum, viverra a eros sit amet, fringilla sagittis auge. Fusce risus tellus, tempus quis lacinia in vehicula ut turpis.</p>
+                <span className='separador'>___________________________</span>
+            <a href='#' className='vagas-link btn'>vagas em aberto &raquo;</a>
+            </div>
         </div>
     )
 }
