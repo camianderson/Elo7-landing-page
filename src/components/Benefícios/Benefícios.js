fe64ba729e5a00511e190299176b49de3a4cae49
Cami Anderson <camidantas@icloud.com>
2023-01-14 17:45:53 -0500

    Adicionar estilização no componente Benefícios

--- src/components/Benefícios/Benefícios.js
+++ src/components/Benefícios/Benefícios.js
@@ -7,24 +7,24 @@ const Benefícios = () => {
     return(
         <div className="benefícios-container">
             <section className="benefícios-container--lista">
-                <section className="sessão">
+                <section className="sessão thin">
                     <img alt="emoji rosto sorrindo" src={qualidade}></img>
-                    <h2>qualidade de vida</h2>
+                    <h2 className="sessão-titulo">qualidade de vida</h2>
                     <p>Lorem, ipsum dolor sit amet, consectetur adipisicing elit. Quisque ut efficitur ipsum risus gravida ex, at malesuada ligula. Pellentesque odio ipsum,</p>
                 </section>
-                <section className="sessão">
+                <section className="sessão thin">
                     <img alt="emoji mão com idicador e mindinho para cima" src={ambiente}></img>
-                    <h2>ambiente descontraído</h2>
+                    <h2 className="sessão-titulo">ambiente descontraído</h2>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ut efficitur ex, at malesuada ligula. Pellentesque odio ipsum, viverra a eros sit amet, fringilla sagittis auge. Fusce risus tellus, tempus quis lacinia in vehicula ut turpis.</p>
                 </section>
-                <section className="sessão">
+                <section className="sessão thin">
                     <img  alt="emoji paleta de pintor"  src={atividades}></img>
-                    <h2>atividades extras</h2>
+                    <h2 className="sessão-titulo">atividades extras</h2>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ut efficitur ex, at malesuada ligula. Pellentesque odio ipsum, viverra a eros sit amet, fringilla sagittis auge. Fusce risus tellus,</p>
                 </section>
             </section>
             <section className="benefícios-container--btn">
-                <span>_____________________________</span>
+                <span className="separador">_____________________________</span>
                 <a href='#' className='saiba-mais-link btn'>Saiba Mais &raquo;</a>
             </section>
         </div>
