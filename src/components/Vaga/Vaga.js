144db10050c1e1127b130a321861b423a9f1823d
Cami Anderson <camidantas@icloud.com>
2023-01-16 15:02:44 -0500

    Desenvolver lógica do componente Vaga

--- src/components/Vaga/Vaga.js
+++ src/components/Vaga/Vaga.js
@@ -1,7 +1,27 @@
-const Vaga = () => {
+const Vaga = ({vagas}) => {
+    const vagasAtivas = vagas.filter(vaga => vaga.ativa);
+
+    const listaDeVagas = vagasAtivas.map(vaga => {
+        if(vaga.localizacao){
+            return(
+                <div key={Math.random()}>
+                    <a href={vaga.link}>{vaga.cargo}</a>
+                    <p>{vaga.localizacao.bairro} - {vaga.localizacao.cidade}, {vaga.localizacao.pais}</p>
+                </div>
+            )
+        } else {
+            return(
+                <div key={Math.random()}>
+                    <a href={vaga.link}>{vaga.cargo}</a>
+                    <p>Remoto</p>
+                </div>
+            )
+        }
+    })
+
     return (
         <div className="vaga-container">
-
+            {listaDeVagas}
         </div>
     )
 }
