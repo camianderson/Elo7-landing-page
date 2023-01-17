e41580a69c6e4e04a05a791dc11f6ef76a933cad
Cami Anderson <camidantas@icloud.com>
2023-01-17 14:12:48 -0500

    Mover API call de App.js para Carreiras.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,10 @@
-import { useState, useEffect } from "react";
 import Carreiras from "./pages/Carreiras/Carreiras";
-import { getVagas } from "./apiCalls";
 
 const App = () => {
 
-  const [dadosVagas, setDadosVagas] = useState([]);
-
-  const getDadosVagas = () => {
-    getVagas()
-    .then(data => {
-      setDadosVagas([...data.vagas])
-    })
-  }
-
-  useEffect(() => {
-    getDadosVagas()
-    console.log(dadosVagas)
-  }, []);
-
-
   return (
     <div className="App">
-      <Carreiras vagas={dadosVagas}/>
+      <Carreiras />
     </div>
   );
 }
