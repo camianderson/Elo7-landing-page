610cd6cda9158b8e75e889ab17c26b12ea8539da
Cami Anderson <camidantas@icloud.com>
2023-01-18 11:42:51 -0500

    Arrumar espaçamento

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import Carreiras from "./pages/Carreiras/Carreiras";
 
 const App = () => {
-
   return (
     <div className="App">
       <Carreiras />
     </div>
   );
-}
+};
 
 export default App;
