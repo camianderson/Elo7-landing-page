4311f8835e01426f56ece154a2fdafcb3463db2c
Cami Anderson <camidantas@icloud.com>
2023-01-18 10:08:28 -0500

    Arrumar espaçamento e nomenclatura do componente Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,23 @@
 const Header = () => {
-    return (
-        <div className='header-container'>
-            <div className='header-container--imagem'>
-                <h1 className='title'>Trabalhe no Elo7</h1>
-            </div>
-            <div className='header-container--descrição'>
-                <p className='descrição'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ut efficitur ex, at malesuada ligula. Pellentesque odio ipsum, viverra a eros sit amet, fringilla sagittis auge. Fusce risus tellus, tempus quis lacinia in vehicula ut turpis.</p>
-                <span className='separador'>___________________________</span>
-            <a href='#' className='vagas-link btn'>vagas em aberto &raquo;</a>
-            </div>
-        </div>
-    )
-}
+  return (
+    <header className="header-container">
+      <div alt="time Elo7" className="header-container--imagem">
+        <h1 className="titulo">Trabalhe no Elo7</h1>
+      </div>
+      <section className="header-container--descrição">
+        <p className="descrição">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ut
+          efficitur ex, at malesuada ligula. Pellentesque odio ipsum, viverra a
+          eros sit amet, fringilla sagittis auge. Fusce risus tellus, tempus
+          quis lacinia in vehicula ut turpis.
+        </p>
+        <span className="separador">___________________________</span>
+        <a href="#" className="vagas-link btn">
+          vagas em aberto &raquo;
+        </a>
+      </section>
+    </header>
+  );
+};
 
 export default Header;
