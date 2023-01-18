46107494efb973a81f91f4819e3d24c31c6c499b
Cami Anderson <camidantas@icloud.com>
2023-01-18 10:13:26 -0500

    Arrumar espaçamento e nomenclatura do componente Vagas

--- src/components/Vagas/Vagas.js
+++ src/components/Vagas/Vagas.js
@@ -1,19 +1,17 @@
-import fotoBottom from '../../assets/foto-bottom.png';
-import Vaga from '../Vaga/Vaga';
+import fotoBottom from "../../assets/foto-bottom.png";
+import Vaga from "../Vaga/Vaga";
 
-const Vagas = ({vagas, erro}) => {
-    return (
-        <div className='vagas-container' >
-            <img className='foto-vagas' alt='feira da elo7' src={fotoBottom}></img>
-            <section className='vagas-container--titulo'>
-                <h2 className='vagas-container--titulo titulo-vagas'>Vagas em aberto</h2>
-            </section>
-            <section className='vagas-container--oportunidades'>
-                <h3 className='titulo-oportunidades'>Desenvolvimento</h3>
-                <Vaga className='vaga' vagas={vagas} erro={erro}/>
-            </section>
-        </div>
-    )
-}
+const Vagas = ({ vagas, erro }) => {
+  return (
+    <div className="vagas-container">
+      <img className="foto-vagas" alt="feira da elo7" src={fotoBottom}></img>
+      <h2 className="vagas-container--titulo">Vagas em aberto</h2>
+      <section className="vagas-container--oportunidades">
+        <h3 className="titulo-oportunidades">Desenvolvimento</h3>
+        <Vaga className="vaga" vagas={vagas} erro={erro} />
+      </section>
+    </div>
+  );
+};
 
 export default Vagas;
