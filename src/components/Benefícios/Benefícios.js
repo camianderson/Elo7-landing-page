9152503e1f3749c5f745a5c53616f7eadf9923f4
Cami Anderson <camidantas@icloud.com>
2023-01-18 10:11:44 -0500

    Arrumar espaçamento e nomenclatura do componente Benefícios

--- src/components/Benefícios/Benefícios.js
+++ src/components/Benefícios/Benefícios.js
@@ -4,31 +4,58 @@ import ambiente from "../../assets/descontracao.png";
 import atividades from "../../assets/atividades.png";
 
 const Benefícios = () => {
-    return(
-        <div className="benefícios-container">
-            <section className="benefícios-container--lista">
-                <section className="sessão thin">
-                    <img alt="emoji rosto sorrindo" src={qualidade}></img>
-                    <h2 className="sessão-titulo">qualidade de vida</h2>
-                    <p>Lorem, ipsum dolor sit amet, consectetur adipisicing elit. Quisque ut efficitur ipsum risus gravida ex, at malesuada ligula. Pellentesque odio ipsum,</p>
-                </section>
-                <section className="sessão thin">
-                    <img alt="emoji mão com idicador e mindinho para cima" src={ambiente}></img>
-                    <h2 className="sessão-titulo">ambiente descontraído</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ut efficitur ex, at malesuada ligula. Pellentesque odio ipsum, viverra a eros sit amet, fringilla sagittis auge. Fusce risus tellus, tempus quis lacinia in vehicula ut turpis.</p>
-                </section>
-                <section className="sessão thin">
-                    <img  alt="emoji paleta de pintor"  src={atividades}></img>
-                    <h2 className="sessão-titulo">atividades extras</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ut efficitur ex, at malesuada ligula. Pellentesque odio ipsum, viverra a eros sit amet, fringilla sagittis auge. Fusce risus tellus,</p>
-                </section>
-            </section>
-            <section className="benefícios-container--btn">
-                <span className="separador">_____________________________</span>
-                <a href='#' className='saiba-mais-link btn'>Saiba Mais &raquo;</a>
-            </section>
+  return (
+    <div className="benefícios-container">
+      <section className="benefícios-container--lista">
+        <div className="sessão thin">
+          <img
+            className="ben-imagem"
+            alt="emoji rosto sorrindo"
+            src={qualidade}
+          ></img>
+          <h2 className="sessão-titulo">qualidade de vida</h2>
+          <p>
+            Lorem, ipsum dolor sit amet, consectetur adipisicing elit. Quisque
+            ut efficitur ipsum risus gravida ex, at malesuada ligula.
+            Pellentesque odio ipsum,
+          </p>
         </div>
-    )
-}
+        <div className="sessão thin">
+          <img
+            className="ben-imagem"
+            alt="emoji mão com idicador e mindinho para cima"
+            src={ambiente}
+          ></img>
+          <h2 className="sessão-titulo">ambiente descontraído</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ut
+            efficitur ex, at malesuada ligula. Pellentesque odio ipsum, viverra
+            a eros sit amet, fringilla sagittis auge. Fusce risus tellus, tempus
+            quis lacinia in vehicula ut turpis.
+          </p>
+        </div>
+        <div className="sessão thin">
+          <img
+            className="ben-imagem"
+            alt="emoji paleta de pintor"
+            src={atividades}
+          ></img>
+          <h2 className="sessão-titulo">atividades extras</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ut
+            efficitur ex, at malesuada ligula. Pellentesque odio ipsum, viverra
+            a eros sit amet, fringilla sagittis auge. Fusce risus tellus,
+          </p>
+        </div>
+      </section>
+      <section className="benefícios-container--btn">
+        <span className="separador">___________________________</span>
+        <a href="#" className="saiba-mais-link btn">
+          Saiba Mais &raquo;
+        </a>
+      </section>
+    </div>
+  );
+};
 
 export default Benefícios;
