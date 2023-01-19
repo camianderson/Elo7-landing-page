992a72e396348d7f8a8b75f64f95d7f2ca766c2a
Cami Anderson <camidantas@icloud.com>
2023-01-19 14:22:04 -0500

    Arrumar erros da pagina quando foi deployed

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,5 +1,5 @@
 const getVagas = () => {
-  return fetch("http://www.mocky.io/v2/5d6fb6b1310000f89166087b").then(
+  return fetch("https://www.mocky.io/v2/5d6fb6b1310000f89166087b").then(
     (response) => response.json()
   );
 };
